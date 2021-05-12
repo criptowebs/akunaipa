bbe41b0db5b45783529822a2143baa171c1356a5
criptowebs <criptowebs@gmail.com>
2021-05-12 15:59:55 -0500

    Create folder theme index and styles mix js and css

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,3 +16,10 @@ mix.js("resources/js/app.js", "public/js").postCss(
     "public/css",
     [require("tailwindcss")]
 );
+
+
+mix.js("resources/js/theme.js", "public/js").postCss(
+    "resources/css/theme.css",
+    "public/css",
+    [require("tailwindcss")]
+);
